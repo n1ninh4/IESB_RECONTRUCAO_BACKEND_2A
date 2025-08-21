fd1a33494403d222af9783dae1b1b78d4c49de7a
n1ninh4 <geovanna.s@iesb.edu.br>
2025-08-20 21:30:55 -0300

    criando meu primeiro NODE

---
+++ praticas/01-primeiro-projeto/index.js
@@ -0,0 +1,17 @@
+console.log ("Rodando meu primeiro projeto em NODE")
+let nome = "geovanna"
+let sobrenome = "maria"
+console.log(nome + " " + sobrenome)
+
+let idade = 21
+
+console.log("A  idade da " + nome + " é " + idade)
+
+
+let  exercicios = 0.5
+let trabalho = 1.5
+let prova = 4.5
+
+let notaA1Calculada = exercicios + trabalho + prova
+
+console.log("NOTA A1: " + notaA1Calculada)
