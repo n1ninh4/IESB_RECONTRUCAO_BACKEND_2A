40e34cf6eee3d485844aecdc2739d44beea5bd22
n1ninh4 <geovanna.s@iesb.edu.br>
2025-10-09 21:46:52 -0300

    atlzd

--- praticas/07-crud-tarefas/index.js
+++ praticas/07-crud-tarefas/index.js
@@ -1,13 +1,20 @@
 const express = require ('express')
-const mongose = require ('mongoose')
-const dotenv = require ('dotenv')
+const mongoose = require ('mongoose')
+const dotenv = require ('dotenv').config()
 
+const app = express()
+app.use(express.json())
 
-const app = express ()
-app.use(express.json)
+const DB_HOST= process.env.DB_HOST
+const DB_USER= process.env.DB_USER
+const DB_PASS= process.env.DB_PASS
+const DB_NAME= process.env.DB_NAME
+
+const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+
 
 //conectra banco 
-mongoose.connect('mongodb+srv://nininha:psoAjhLPGjWorpoL@cluster0.h8wdaif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(url)
 
 .then(() => {
     console.log("contado ao mongoDB")
